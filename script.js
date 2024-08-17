c0cbdb3f021baed0d5715fe64d4e233d59300b90
Angelina Zhou <zhouangelina3@gmail.com>
2024-08-17 18:35:30 -0400

    Added functionality to switch between black and rainbow fill colors for grid boxes.
    When the 'Rainbow' button  is selected, hovering over a box changes it to a random color.

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-const gridLength = 512;
-const body = document.querySelector("body");
-
 function buildGrid (numRows, numCols){
-    const gridContainer = document.createElement("div");
-    gridContainer.classList.add("container");
-    body.appendChild(gridContainer);
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+    gridContainer.appendChild(grid);
     
     for (let i = 0; i < numRows; i++){
         const row = document.createElement("div");
@@ -14,26 +11,51 @@ function buildGrid (numRows, numCols){
             box.classList.add("box")
             box.style.width = `${gridLength / numCols}px`
             box.style.height = `${gridLength / numRows}px`
-            box.addEventListener("mouseover", () => {
-                box.style.backgroundColor = "black";
-            })
+            box.addEventListener("mouseover", fillBlack)
             row.appendChild(box)
         }
-        gridContainer.appendChild(row)
+        grid.appendChild(row)
     }
 }
 
 function removeGrid (){
-    const gridContainer = document.querySelector(".container")
-    body.removeChild(gridContainer)
+    const grid = document.querySelector(".grid")
+    gridContainer.removeChild(grid)
 }
 
-buildGrid(16,16);
+function fillBlack(e){
+    e.target.style.backgroundColor = "black";
+}
+
+function fillRainbow(e){
+    e.target.style.backgroundColor = getRandomRGBColor();
+}
+
+function getRandomRGBColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+
+const DEFAULT_GRID_ROWS = 16;
+const DEFAULT_GRID_COLS = 16;
+const MAX_GRID_SIZE = 100
+
+const gridLength = 512;
+const gridContainer = document.querySelector(".gridContainer")
 
 const inputRow = document.querySelector(".inputRow");
 const inputCol = document.querySelector(".inputCol");
 
+buildGrid(DEFAULT_GRID_ROWS, DEFAULT_GRID_COLS);
+
 const newGridButton = document.querySelector("button.newGrid");
+const rainbowButton = document.querySelector("button.rainbow");
+const blackButton = document.querySelector("button.black");
+
+
 newGridButton.addEventListener ("click", () => {
     if (inputRow.value === "" || inputCol.value === ""){
         alert("Enter grid dimensions")
@@ -46,14 +68,34 @@ newGridButton.addEventListener ("click", () => {
         return;
     }
     removeGrid();
-    if (inputRow.value > 100) {
-        inputRow.value = 100;
-    }
-
-    if (inputCol.value > 100) {
-        inputCol.value = 100;
-    }
+    inputRow.value = Math.min(inputRow.value, MAX_GRID_SIZE)
+    inputCol.value = Math.min(inputCol.value, MAX_GRID_SIZE)
 
     buildGrid(inputRow.value, inputCol.value);
     
 })
+
+let colorBlack = true;
+
+rainbowButton.addEventListener("click", () => {
+    if (colorBlack) {
+        const boxes = document.querySelectorAll(".box")
+        boxes.forEach((box) => {
+            box.removeEventListener("mouseover", fillBlack);
+            box.addEventListener("mouseover", fillRainbow)
+        })
+        colorBlack = false;
+    }
+})
+
+blackButton.addEventListener("click", () => {
+    if (!colorBlack) {
+        const boxes = document.querySelectorAll(".box")
+        boxes.forEach((box) => {
+            box.removeEventListener("mouseover", fillRainbow);
+            box.addEventListener("mouseover", fillBlack);
+        colorBlack = true;
+    })
+    } 
+})
+
